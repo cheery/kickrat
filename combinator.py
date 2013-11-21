df325f41340cb1a1991093b866fb9a54d94bb99b
Henri Tuhola <cheery@boxbase.org>
2013-11-21 22:20:25 +0200

    primitive error reporting

--- combinator.py
+++ combinator.py
@@ -20,6 +20,39 @@ class result(object):
     def string(self):
         return ''.join(value.string for value in self)
 
+class expect(object):
+    def __init__(self, pos, terminals):
+        self.terminals = set(terminals)
+        self.pos = pos
+
+    def __add__(self, other):
+        if other is None:
+            return self
+        if self.pos < other.pos:
+            return other
+        if self.pos > other.pos:
+            return self
+        return expect(self.pos, self.terminals | other.terminals)
+
+    def __lshift__(self, other):
+        if other is None:
+            return self
+        if other.pos <= self.pos:
+            return self
+        else:
+            return other
+
+    def __repr__(self):
+        return ' or '.join(str(o) for o in self.terminals)
+
+def getpos(source, index):
+    if index < len(source):
+        return source[index].start
+    if len(source) > 0:
+        return source[len(source)-1].stop
+    else:
+        return 0
+
 class label(object):
     def __init__(self, name):
         self.name = name
@@ -35,6 +68,7 @@ class near(object):
     def match(self, source, index):
         if index < len(source):
             if not source[index].near:
+                i = source[index].start
                 return -1, None
         return match(self.subject, source, index)
 
@@ -42,16 +76,22 @@ class near(object):
 class choice(object):
     def __init__(self, *choices):
         self.choices = list(choices)
+        self.name = None
 
     def append(self, choice):
         self.choices.append(choice)
 
     def match(self, source, index):
+        ex = expect(getpos(source, index), [])
         for obj in self.choices:
             k, r = match(obj, source, index)
             if k >= 0:
                 return k, r
-        return -1, None
+            else:
+                ex = ex + r
+        if self.name:
+            ex = expect(getpos(source, index), [self.name]) << ex
+        return -1, ex
 
 class star(object):
     def __init__(self, subject):
@@ -84,7 +124,7 @@ class plus(object):
         res = result(self.mark)
         k, r = match(self.subject, source, index+count)
         if k < 0:
-            return -1, None
+            return -1, r
         while k >= 0:
             res.append(r)
             count += k
@@ -100,6 +140,7 @@ class sequence(object):
     def __init__(self, *objects):
         self.objects = objects
         self.mark = self
+        self.name = None
 
     def match(self, source, index):
         count = 0
@@ -109,8 +150,10 @@ class sequence(object):
             if k >= 0:
                 res.append(r)
                 count += k
+            elif self.name:
+                return -1, expect(getpos(source, index), [self.name]) << r
             else:
-                return -1, None
+                return -1, r
         res.start = source[index].start
         res.stop  = source[index+count-1].stop
         return count, res
@@ -128,10 +171,13 @@ class lookahead(object):
         if self.positive:
             if k >= 0:
                 return 0, res
+            else:
+                return -1, res
         else:
             if k < 0:
                 return 0, res
-        return -1, None
+            else:
+                return -1, None
 
 class optional(object):
     def __init__(self, subject):
@@ -149,7 +195,7 @@ def match_string(string, source, index):
     if index < len(source):
         if source[index].string == string:
             return 1, source[index]
-    return -1, None
+    return -1, expect(getpos(source, index), [repr(string)])
 
 def match(obj, source, index):
     if isinstance(obj, (str, unicode)):
@@ -158,7 +204,7 @@ def match(obj, source, index):
         if len(source) == index:
             return 0, None
         else:
-            return -1, None
+            return -1, expect(getpos(source, index), ['eof'])
     return obj.match(source, index)
 
 def parse(root, source):
